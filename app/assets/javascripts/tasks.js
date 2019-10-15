fba53fcb9376da59df8a4a3e5a23ef11dd337429
fukutome-yuta <44062419+fukutome-yuta@users.noreply.github.com>
2019-10-15 12:21:00 +0900

    Update tasks.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,14 @@
+//turbolinkが適用されていた場合はwindow.onloadではなくこれでページ読み込み時に処理が実行される
 document.addEventListener('turbolinks:load', function() {
+  //すべてのtdタグを取得し、すべてに適用する
   document.querySelectorAll('td').forEach(function(td) {
+    //マウスオーバー時の処理を追加
     td.addEventListener('mouseover', function(e) {
+      //currentTarget.styleでイベントを検知したターゲットだけにスタイルを適用する
       e.currentTarget.style.backgroundColor = '#eff';
     });
 
+    //マウスアウト時の処理
     td.addEventListener('mouseout', function(e) {
       e.currentTarget.style.backgroundColor = '';
     });
